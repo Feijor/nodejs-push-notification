c0995c58b7c580a6e1a925c4764186205307f784
DESKTOP-RA1Q8MK\Feijor <thiago@feijor.com.br>
2017-09-13 16:31:04 -0300

    add index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const Model = require('../../lib/facade');
 var gcm = require('node-gcm');
+var apn = require('apn');
 var tokensAndroid = [];
-var tokensIOS;
-var senderID = 'your_sender_ID';
+var tokensIOS = [];
+var senderID = 'your_senderID';
 
-class pushoModel extends Model {
+class pushoModel {
   
   addToken(token,platform) {
-    if(platform.toLowerCase() == 'android'){
-      tokensAndroid.push(token);
-    }
-    
-    if(platform.toLowerCase() == 'ios'){
-      tokensIOS = tokensIOS + ' ' + token;
+    if(platform){
+      if(platform.toLowerCase() == 'android'){
+        tokensAndroid.push(token);
+      }
+      
+      if(platform.toLowerCase() == 'ios'){
+        tokensIOS.push(token);
+      }
     }
   }
   
   send(messagep){
     var msgAndroid = {'title': messagep.titulo, 'body': messagep.msg, 'icon':'ic_launcher'};
-    var msgIos = {'title': messagep.titulo, 'body': messagep.msg, 'icon':'ic_launcher'};
 
     if(tokensAndroid.length > 0){
       var message = new gcm.Message({
@@ -35,6 +36,38 @@ class pushoModel extends Model {
         if(err) console.error(err);
         else    console.log(response);
       });
+    }
+
+    if(tokensIOS){
+
+      tokensIOS = Array.from(new Set(tokensIOS));
+
+      let service = new apn.Provider({
+        cert: "./lib/cert.pem",
+        key: "./lib/key.pem",
+        passphrase: 'your_pass',
+        production: true
+      });
+      
+      let note = new apn.Notification();
+      
+      note.topic = "your_bounder_id";
+      note.expiry = Math.floor(Date.now() / 1000) + 3600;
+      note.badge = 3;
+      note.sound = "ping.aiff";
+      note.alert = messagep.titulo; 
+      note.body = messagep.msg;
+      note.title = messagep.titulo;
+      note.payload = {'messageFrom': 'teste'};
+
+      console.log(`Sending: ${note.compile()} to ${tokensIOS}`);
+      service.send(note, tokensIOS).then( result => {
+          console.log("sent:", result.sent.length);
+          console.log("failed:", result.failed.length);
+          console.log(result.failed);
+      });
+
+      service.shutdown();
     }
   }
 }
